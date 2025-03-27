b1ddba9a91073176310f7590acc82afe160310c4
chourafiDev <abdelmonaimechourafi@gmail.com>
2025-03-27 12:19:10 +0000

    Fix code

--- app/components/Product/Product.tsx
+++ app/components/Product/Product.tsx
@@ -1,74 +1,79 @@
 import Image from "next/image";
-import { client4, client11, client5 } from "../../../assets";
-import { Product } from "../../../types";
-import ProfileTooltip from "../ProfileTooltip/ProfileTooltip";
-import { BiDotsHorizontalRounded, BiHeart } from "react-icons/bi";
 import Link from "next/link";
+import { BiDotsHorizontalRounded, BiHeart } from "react-icons/bi";
 import { Timer } from "..";
+import { client4, client5, client11 } from "../../../assets";
+import { Product } from "../../../types";
 
 interface IProps {
-  product: Product;
-  timeStamp?: number;
+	product: Product;
+	timeStamp?: number;
 }
 
 const Product = ({ product, timeStamp }: IProps) => {
-  return (
-    <div className="bg-[#242435] p-1 rounded-lg product group">
-      <div className="relative">
-        <div className="rounded-lg overflow-hidden">
-          <Image
-            src={product.cover}
-            alt={product.title}
-            className="transform group-hover:scale-110 duration-700 ease-liner object-cover"
-            width={500}
-            height={500}
-          />
-        </div>
-        <div className="absolute bottom-4 left-[3px] w-full flex justify-center">
-          {timeStamp && <Timer countDownTimestampMS={timeStamp} />}
-        </div>
-      </div>
+	return (
+		<Link href="/product-details/1">
+			<div className="bg-[#242435] p-1 rounded-lg product group">
+				<div className="relative">
+					<div className="rounded-lg overflow-hidden">
+						<Image
+							src={product.cover}
+							alt={product.title}
+							className="transform group-hover:scale-110 duration-700 ease-liner object-cover"
+							width={500}
+							height={500}
+						/>
+					</div>
+					<div className="absolute bottom-4 left-[3px] w-full flex justify-center">
+						{timeStamp && <Timer countDownTimestampMS={timeStamp} />}
+					</div>
+				</div>
 
-      <div className="px-5 pb-5">
-        <div className="flex items-center justify-between my-4 text-sm">
-          <div className="flex gap-2 items-center">
-            <div className="flex items-center -space-x-2">
-              <div className="hover:transform hover:scale-125 duration-200 ease-in cursor-pointer hover:z-30 border-2 w-[30px] h-[30px] border-white/20 rounded-full">
-                <Image src={client4} alt="client4" className="rounded-full" />
-              </div>
-              <div className="hover:transform hover:scale-125 duration-200 ease-in cursor-pointer hover:z-30 border-2 w-[30px] h-[30px] border-white/20 rounded-full">
-                <Image src={client5} alt="client5" className="rounded-full" />
-              </div>
-              <div className="hover:transform hover:scale-125 duration-200 ease-in cursor-pointer hover:z-30 border-2 w-[30px] h-[30px] border-white/20 rounded-full">
-                <Image src={client11} alt="client11" className="rounded-full" />
-              </div>
-            </div>
-            <p className="text-light font-medium">16+ Place Bit</p>
-          </div>
-          <BiDotsHorizontalRounded
-            size={35}
-            className="text-light cursor-pointer hover:bg-primary/10 duration-200 ease-out rounded-full p-2"
-          />
-        </div>
-        <Link href="#">
-          <a className="text-white font-medium text-lg duration-200 ease-in title">
-            {product["propertie-name"]}
-          </a>
-        </Link>
+				<div className="px-5 pb-5">
+					<div className="flex items-center justify-between my-4 text-sm">
+						<div className="flex gap-2 items-center">
+							<div className="flex items-center -space-x-2">
+								<div className="hover:transform hover:scale-125 duration-200 ease-in cursor-pointer hover:z-30 border-2 w-[30px] h-[30px] border-white/20 rounded-full">
+									<Image src={client4} alt="client4" className="rounded-full" />
+								</div>
+								<div className="hover:transform hover:scale-125 duration-200 ease-in cursor-pointer hover:z-30 border-2 w-[30px] h-[30px] border-white/20 rounded-full">
+									<Image src={client5} alt="client5" className="rounded-full" />
+								</div>
+								<div className="hover:transform hover:scale-125 duration-200 ease-in cursor-pointer hover:z-30 border-2 w-[30px] h-[30px] border-white/20 rounded-full">
+									<Image
+										src={client11}
+										alt="client11"
+										className="rounded-full"
+									/>
+								</div>
+							</div>
+							<p className="text-light font-medium">16+ Place Bit</p>
+						</div>
+						<BiDotsHorizontalRounded
+							size={35}
+							className="text-light cursor-pointer hover:bg-primary/10 duration-200 ease-out rounded-full p-2"
+						/>
+					</div>
+					<Link href="#">
+						<a className="text-white font-medium text-lg duration-200 ease-in title">
+							{product["propertie-name"]}
+						</a>
+					</Link>
 
-        <p className="text-light">Highest bit 1/20</p>
-        <div className="flex items-center justify-between mt-3">
-          <p className="text-primary text-sm font-medium">
-            {product.price}wETH
-          </p>
-          <p className="flex items-center gap-1 text-light duration-300 ease-in hover:bg-primary hover:text-white px-2 rounded-md cursor-pointer">
-            <BiHeart />
-            <span>99</span>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+					<p className="text-light">Highest bit 1/20</p>
+					<div className="flex items-center justify-between mt-3">
+						<p className="text-primary text-sm font-medium">
+							{product.price}wETH
+						</p>
+						<p className="flex items-center gap-1 text-light duration-300 ease-in hover:bg-primary hover:text-white px-2 rounded-md cursor-pointer">
+							<BiHeart />
+							<span>99</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default Product;
--- app/core/NavBar/NavLinks.tsx
+++ app/core/NavBar/NavLinks.tsx
@@ -4,163 +4,163 @@ import { useState } from "react";
 import { RiArrowDownSLine } from "react-icons/ri";
 
 import {
-  allnfts,
-  collectibles,
-  domainNames,
-  music,
-  photography,
-  solana,
-  sports,
-  trading,
-  utility,
-  virtualWorlds,
-  art,
-  nft1,
-  nft2,
-  nft3,
-  nft4,
-  nft5,
-  nft6,
-  nft7,
+	allnfts,
+	art,
+	collectibles,
+	domainNames,
+	music,
+	nft1,
+	nft2,
+	nft3,
+	nft4,
+	nft5,
+	nft6,
+	nft7,
+	photography,
+	solana,
+	sports,
+	trading,
+	utility,
+	virtualWorlds,
 } from "../../../assets";
 
 const NavLinks = () => {
-  const [isHover, setIsHover] = useState(false);
+	const [isHover, setIsHover] = useState(false);
 
-  const links = [
-    {
-      link: "/",
-      name: "Home",
-      submenu: true,
-      type: "images",
-      images: [nft1, nft2, nft3, nft4, nft5, nft6, nft7, nft2, nft4],
-    },
-    {
-      link: "/",
-      name: "Explore",
-      submenu: true,
-      type: "links",
-      sublinks: [
-        { subLink: "/", name: "ALL NFTs", icon: allnfts },
-        { subLink: "/", name: "Solana NFTs", icon: solana },
-        { subLink: "/", name: "Art", icon: art },
-        { subLink: "/", name: "Collectibles", icon: collectibles },
-        { subLink: "/", name: "Domain Names", icon: domainNames },
-        { subLink: "/", name: "Music", icon: music },
-        { subLink: "/", name: "Photography", icon: photography },
-        { subLink: "/", name: "Sports", icon: sports },
-        { subLink: "/", name: "Trading Cards", icon: trading },
-        { subLink: "/", name: "Utility", icon: utility },
-        { subLink: "/", name: "Virtual Worlds", icon: virtualWorlds },
-      ],
-    },
-    {
-      link: "/",
-      name: "Status",
-      submenu: true,
-      type: "links",
-      sublinks: [
-        { subLink: "/", name: "Rankings", icon: "" },
-        { subLink: "/", name: "Activity", icon: "" },
-      ],
-    },
-    {
-      link: "/",
-      name: "Resources",
-      submenu: true,
-      type: "links",
-      sublinks: [
-        { subLink: "/", name: "Learn", icon: "" },
-        { subLink: "/", name: "Help Center", icon: "" },
-        { subLink: "/", name: "Platform Status", icon: "" },
-        { subLink: "/", name: "Partners", icon: "" },
-        { subLink: "/", name: "Taxes", icon: "" },
-        { subLink: "/", name: "Blogs", icon: "" },
-        { subLink: "/", name: "Docs", icon: "" },
-        { subLink: "/", name: "Newsletter", icon: "" },
-      ],
-    },
-    { link: "/", name: "Create" },
-  ];
+	const links = [
+		{
+			link: "/",
+			name: "Home",
+			submenu: true,
+			type: "images",
+			images: [nft1, nft2, nft3, nft4, nft5, nft6, nft7, nft2, nft4],
+		},
+		{
+			link: "/",
+			name: "Explore",
+			submenu: true,
+			type: "links",
+			sublinks: [
+				{ subLink: "/", name: "ALL NFTs", icon: allnfts },
+				{ subLink: "/", name: "Solana NFTs", icon: solana },
+				{ subLink: "/", name: "Art", icon: art },
+				{ subLink: "/", name: "Collectibles", icon: collectibles },
+				{ subLink: "/", name: "Domain Names", icon: domainNames },
+				{ subLink: "/", name: "Music", icon: music },
+				{ subLink: "/", name: "Photography", icon: photography },
+				{ subLink: "/", name: "Sports", icon: sports },
+				{ subLink: "/", name: "Trading Cards", icon: trading },
+				{ subLink: "/", name: "Utility", icon: utility },
+				{ subLink: "/", name: "Virtual Worlds", icon: virtualWorlds },
+			],
+		},
+		{
+			link: "/",
+			name: "Status",
+			submenu: true,
+			type: "links",
+			sublinks: [
+				{ subLink: "/", name: "Rankings", icon: "" },
+				{ subLink: "/", name: "Activity", icon: "" },
+			],
+		},
+		{
+			link: "/",
+			name: "Resources",
+			submenu: true,
+			type: "links",
+			sublinks: [
+				{ subLink: "/", name: "Learn", icon: "" },
+				{ subLink: "/", name: "Help Center", icon: "" },
+				{ subLink: "/", name: "Platform Status", icon: "" },
+				{ subLink: "/", name: "Partners", icon: "" },
+				{ subLink: "/", name: "Taxes", icon: "" },
+				{ subLink: "/", name: "Blogs", icon: "" },
+				{ subLink: "/", name: "Docs", icon: "" },
+				{ subLink: "/", name: "Newsletter", icon: "" },
+			],
+		},
+		{ link: "/create", name: "Create" },
+	];
 
-  return (
-    <>
-      {links.map((link) => (
-        <li key={link.name} className="relative group">
-          <Link href={link.link}>
-            {link.submenu ? (
-              <a className="hover:text-white flex items-center gap-1 duration-200 ease-linear">
-                <p>{link.name}</p>
-                <RiArrowDownSLine />
-              </a>
-            ) : (
-              <a className="hover:text-white duration-200 ease-linear">
-                {link.name}
-              </a>
-            )}
-          </Link>
+	return (
+		<>
+			{links.map((link) => (
+				<li key={link.name} className="relative group">
+					<Link href={link.link}>
+						{link.submenu ? (
+							<a className="hover:text-white flex items-center gap-1 duration-200 ease-linear">
+								<p>{link.name}</p>
+								<RiArrowDownSLine />
+							</a>
+						) : (
+							<a className="hover:text-white duration-200 ease-linear">
+								{link.name}
+							</a>
+						)}
+					</Link>
 
-          {link.submenu && link.type === "links" ? (
-            <div
-              className={`bg-transparent pt-6 absolute top-3 hidden hover:block group-hover:block duration-300 ease-linear`}
-            >
-              <ul className="custome-scrollbar navbar-scrollbar max-h-[34rem] w-64 group-hover:opacity-100 hover:opacity-100 group-hover:translate-y-4 opacity-0 overflow-y-scroll p-4 rounded-lg border border-white/10 bg-[#181822] lg:flex flex-col gap-4 text-light font-medium text-lg">
-                {link.sublinks?.map((el) => (
-                  <li key={el.name}>
-                    <Link href={el.subLink}>
-                      {el.icon ? (
-                        <a className="flex gap-4 items-center">
-                          <Image
-                            src={el.icon}
-                            width={30}
-                            height={30}
-                            alt={el.subLink}
-                          />
-                          <div className="bg-white/10 h-6 w-[1px] lg:block hidden"></div>
-                          <p className="hover:text-white duration-200 ease-linear">
-                            {el.name}
-                          </p>
-                        </a>
-                      ) : (
-                        <a className="hover:text-white duration-200 ease-linear">
-                          {el.name}
-                        </a>
-                      )}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ) : link.submenu && link.type === "images" ? (
-            <div
-              className={`bg-transparent pt-6 absolute top-7 hidden hover:block group-hover:block duration-300 ease-linear`}
-            >
-              <div className="custome-scrollbar w-[470%] overflow-y-scroll p-4 rounded-lg border border-white/10 bg-[#181822]">
-                <h2 className="text-white font-semibold text-xl">Top NFTs</h2>
-                <ul className="flex flex-wrap gap-2 mt-6">
-                  {link.images?.map((image, i) => (
-                    <li key={i}>
-                      <Link href="/">
-                        <a>
-                          <Image
-                            src={image}
-                            width={110}
-                            height={110}
-                            alt="NFT"
-                            className="rounded-lg"
-                          />
-                        </a>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ) : null}
-        </li>
-      ))}
-    </>
-  );
+					{link.submenu && link.type === "links" ? (
+						<div
+							className={`bg-transparent pt-6 absolute top-3 hidden hover:block group-hover:block duration-300 ease-linear`}
+						>
+							<ul className="custome-scrollbar navbar-scrollbar max-h-[34rem] w-64 group-hover:opacity-100 hover:opacity-100 group-hover:translate-y-4 opacity-0 overflow-y-scroll p-4 rounded-lg border border-white/10 bg-[#181822] lg:flex flex-col gap-4 text-light font-medium text-lg">
+								{link.sublinks?.map((el) => (
+									<li key={el.name}>
+										<Link href={el.subLink}>
+											{el.icon ? (
+												<a className="flex gap-4 items-center">
+													<Image
+														src={el.icon}
+														width={30}
+														height={30}
+														alt={el.subLink}
+													/>
+													<div className="bg-white/10 h-6 w-[1px] lg:block hidden"></div>
+													<p className="hover:text-white duration-200 ease-linear">
+														{el.name}
+													</p>
+												</a>
+											) : (
+												<a className="hover:text-white duration-200 ease-linear">
+													{el.name}
+												</a>
+											)}
+										</Link>
+									</li>
+								))}
+							</ul>
+						</div>
+					) : link.submenu && link.type === "images" ? (
+						<div
+							className={`bg-transparent pt-6 absolute top-7 hidden hover:block group-hover:block duration-300 ease-linear`}
+						>
+							<div className="custome-scrollbar w-[470%] overflow-y-scroll p-4 rounded-lg border border-white/10 bg-[#181822]">
+								<h2 className="text-white font-semibold text-xl">Top NFTs</h2>
+								<ul className="flex flex-wrap gap-2 mt-6">
+									{link.images?.map((image, i) => (
+										<li key={i}>
+											<Link href="/">
+												<a>
+													<Image
+														src={image}
+														width={110}
+														height={110}
+														alt="NFT"
+														className="rounded-lg"
+													/>
+												</a>
+											</Link>
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+					) : null}
+				</li>
+			))}
+		</>
+	);
 };
 
 export default NavLinks;
